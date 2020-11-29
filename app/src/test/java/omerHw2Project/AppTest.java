2fb9ce0a0e52142b1a4cdc2a59f51551ee429092
omerfarukpolat <polatfaruk06@gmail.com>
2020-11-29 13:10:40 +0300

    Getting ready for deployment to heroku

--- app/src/test/java/omerHw2Project/AppTest.java
+++ app/src/test/java/omerHw2Project/AppTest.java
@@ -36,5 +36,67 @@ class AppTest {
     public void testNull() {
       assertFalse(App.search(null, 1));
     }
-
+ @Test
+    public void objectsEqual() {
+        ArrayList<Integer> arg = new ArrayList<>();
+        arg.add(1);
+        arg.add(2);
+        arg.add(3);
+          assertThrows(
+                  RuntimeException.class,
+                  () -> { App.swap(arg, 1, 1); });
+    }
+    @Test
+    public void cannotFindX() {  
+        ArrayList<Integer> arg = new ArrayList<>();
+        arg.add(1);
+        arg.add(2);
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        expected.add(2);
+        assertTrue(expected.equals(App.swap(arg,3,1)));
+    }
+    @Test
+    public void cannotFindY() { 
+        ArrayList<Integer> arg = new ArrayList<>();
+        arg.add(1);
+        arg.add(2);
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        expected.add(2);
+        assertTrue(expected.equals(App.swap(arg,1,3)));
+    }
+    @Test
+    public void swapWithFirstX() {
+        ArrayList<Integer> arg = new ArrayList<>();
+        arg.add(1);
+        arg.add(2);
+        arg.add(3);
+        arg.add(2);
+        arg.add(4);
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        expected.add(4);
+        expected.add(3);
+        expected.add(2);
+        expected.add(2);
+        assertTrue(expected.equals(App.swap(arg,2,4)));
+    }
+    @Test
+    public void swapWithLastY() {
+        ArrayList<Integer> arg = new ArrayList<>();
+        arg.add(1);
+        arg.add(2);
+        arg.add(3);
+        arg.add(2);
+        arg.add(4);
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(2);
+        expected.add(2);
+        expected.add(3);
+        expected.add(1);
+        expected.add(4);
+        assertTrue(expected.equals(App.swap(arg,1,2)));
+        
+      }
 }
